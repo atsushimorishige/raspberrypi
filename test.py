26962973d664ffb70510014ad1fdee43ba0a4a6a
atsushimorishige <netdex.ms@gmail.com>
2014-10-02 17:43:26 +0900

    modify 2

--- test.py
+++ test.py
@@ -4,9 +4,11 @@
 #m = MeCab.Tagger()
 #print m.parse("おはよう、RaspberryPiがやっときましたよ。いえー。")
 
-import eyed3
-
-audiofile = eyed3.load("./share/TBS_2014-10-02-0813.mp3")
-audiofile.tag.artist = (u"テスト")
-audiofile.tag.artist = (u"アルバム名")
-audiofile.tag.artist = (u"タイトル")
+import eyeD3
+tag=eyeD3.Tag()
+tag.link("/home/pi/share/TBS_2014-10-02-0813.mp3")
+tag.header.setVersion(eyeD3.ID3_V2_3);
+tag.setTextEncoding(eyeD3.UTF_16_ENCODING);
+tag.setTitle(u"テスト")
+tag.setArtist(u"テストw")
+tag.update()
