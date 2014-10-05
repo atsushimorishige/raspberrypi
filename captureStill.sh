42a7c9d059ddce09e3590c2e363a734888366ff0
atsushimorishige <netdex.ms@gmail.com>
2014-10-05 12:55:44 +0900

    new file:   captureStill.sh 	new file:   uploadTW.sh

---
+++ captureStill.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/bin/raspistill -w 1024 -h 768 -q 100 -o /home/pi/share/image/dailycapture.jpg
---
+++ uploadTW.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/local/bin/tw "定点カメラ 川崎の風景　`/bin/date`" --file=/home/pi/share/image/dailycapture.jpg --yes
